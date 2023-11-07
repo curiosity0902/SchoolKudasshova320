87c4317522c68d5a14a11b1aba678883728c4177
Анна <annakudashova0902@gmail.com>
2023-11-07 21:31:27 +0300

    Нерабочий айдишник.

--- SchoolKudasshova320/Pages/AddDepartmentPage.xaml.cs
+++ SchoolKudasshova320/Pages/AddDepartmentPage.xaml.cs
@@ -22,16 +22,14 @@ namespace SchoolKudasshova320.Pages
     /// </summary>
     public partial class AddDepartmentPage : Page
     {
-        public static List<Chair> chairs = new List<Chair>();
-        public static List<Discipline> disciplines = new List<Discipline>();
+        public static List<Chair> chairs { get; set; }
+        public static List<Discipline> disciplines { get; set; }
 
-        public static Chair cha { get; set; }
         Chair contextChair;
         public AddDepartmentPage(Chair chair)
         {
             InitializeComponent();
             contextChair = chair;
-            cha = chair;
             InitializeDataInPage();
             this.DataContext = this;
 
@@ -39,12 +37,18 @@ namespace SchoolKudasshova320.Pages
 
         private void InitializeDataInPage()
         {
-            CBDisp.ItemsSource = DBConnection.practise320_KudashovaAnnaEntities.Chair.ToList();
-            chairs = new List<Chair> (DBConnection.practise320_KudashovaAnnaEntities.Chair.ToList());
-            disciplines = new List<Discipline>(DBConnection.practise320_KudashovaAnnaEntities.Discipline.Where(x => x.ID_Chair == contextChair.ID).ToList());
+            chairs = new List<Chair>(DBConnection.practise320_KudashovaAnnaEntities.Chair.
+       Where(i => i.Name == contextChair.Name).ToList());
+            AddDepartmentLv.ItemsSource = DBConnection.practise320_KudashovaAnnaEntities.Discipline.Where(x => x.ID_Chair == contextChair.ID).ToList();
+            TBNameChair.Text = Convert.ToString(contextChair.Name);
+            disciplines = new List<Discipline>(DBConnection.practise320_KudashovaAnnaEntities.Discipline.ToList());
 
         }
-
+        private void Refresh() //Обновление листа
+        {
+            AddDepartmentLv.ItemsSource = DBConnection.practise320_KudashovaAnnaEntities.Discipline.
+                            Where(i => i.ID_Chair == contextChair.ID).ToList();
+        }
         private void ExitBT_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.GoBack();
@@ -62,51 +66,56 @@ namespace SchoolKudasshova320.Pages
                 DBConnection.practise320_KudashovaAnnaEntities.Discipline.Remove(discipline);
                 DBConnection.practise320_KudashovaAnnaEntities.SaveChanges();
                 InitializeDataInPage();
+                Refresh();
             }
         }
 
-
-        public void Refresh()
-        {
-            AddDepartmentLv.ItemsSource = DBConnection.practise320_KudashovaAnnaEntities.Discipline.ToList();
-        }
 
         
         private void AddDisp_Click(object sender, RoutedEventArgs e)
         {
+            Discipline disc = new Discipline();
+
             if (CBDisp.SelectedItem is Discipline discipline)
             {
-
-                var error = string.Empty;
-                var validationContext = new ValidationContext(contextChair);
-                var results = new List<System.ComponentModel.DataAnnotations.ValidationResult>();
-                var disciplina = DBConnection.practise320_KudashovaAnnaEntities.Discipline.FirstOrDefault(x => x.ID_Chair == contextChair.ID);
-                //if (disciplina != null && disciplina != contextChair)
-                //    error += "Такое уже есть";
-                if (Validator.TryValidateObject(contextChair, validationContext, results, true))
-                {
-                    foreach (var result in results)
-                    {
-                        error += $"{result.ErrorMessage}\n";
-                    }
-                }
-                if (!string.IsNullOrWhiteSpace(error))
-                {
-                    MessageBox.Show(error);
-                    return;
-                }
-                if (discipline == null)
-                    DBConnection.practise320_KudashovaAnnaEntities.Discipline.Add(discipline);
+                var d = CBDisp.SelectedItem as Discipline;
+                disc.Name = d.Name;
+                disc.Volume = Convert.ToInt32(TBVolume.Text);
+                disc.ID = Convert.ToString(contextChair.ID);
+                DBConnection.practise320_KudashovaAnnaEntities.Discipline.Add(disc);
                 DBConnection.practise320_KudashovaAnnaEntities.SaveChanges();
+                Refresh();
+                InitializeDataInPage();
+                //var error = string.Empty;
+                //var validationContext = new ValidationContext(contextChair);
+                //var results = new List<System.ComponentModel.DataAnnotations.ValidationResult>();
+                //var disciplina = DBConnection.practise320_KudashovaAnnaEntities.Discipline.FirstOrDefault(x => x.ID_Chair == contextChair.ID);
+                ////if (disciplina != null && disciplina != contextChair)
+                ////    error += "Такое уже есть";
+                //if (Validator.TryValidateObject(contextChair, validationContext, results, true))
+                //{
+                //    foreach (var result in results)
+                //    {
+                //        error += $"{result.ErrorMessage}\n";
+                //    }
+                //}
+                //if (!string.IsNullOrWhiteSpace(error))
+                //{
+                //    MessageBox.Show(error);
+                //    return;
+                //}
+                //if (discipline == null)
+                //    DBConnection.practise320_KudashovaAnnaEntities.Discipline.Add(discipline);
+                //DBConnection.practise320_KudashovaAnnaEntities.SaveChanges();
 
             }
         }
 
         
 
-        private void Page_Loaded_1(object sender, RoutedEventArgs e)
+        private void ChangeBTN_Click(object sender, RoutedEventArgs e)
         {
-            Refresh();
+            NavigationService.Navigate(new AddChairPage());
         }
     }
 }
--- SchoolKudasshova320/Pages/ChairPage.xaml.cs
+++ SchoolKudasshova320/Pages/ChairPage.xaml.cs
@@ -23,19 +23,18 @@ namespace SchoolKudasshova320.Pages
     {
         public static List<Chair> chairs { get; set; }
         public static List<Worker> workers { get; set; }
-        public static List<Discipline> disciplines { get; set; }
         public static List<Faculty> faculties { get; set; }
 
         public static Worker loggedUser;
-        public static Chair chair1 = new Chair();
+
         public ChairPage()
         {
             InitializeComponent();
-            faculties = new List<Faculty>(DBConnection.practise320_KudashovaAnnaEntities.Faculty.ToList());
-            disciplines = new List<Discipline>(DBConnection.practise320_KudashovaAnnaEntities.Discipline.ToList());
-            workers = new List<Worker>(DBConnection.practise320_KudashovaAnnaEntities.Worker.ToList());
-            chairs = DBConnection.practise320_KudashovaAnnaEntities.Chair.Where(x => x.ID == DBConnection.loginedUser.ID_Chair).ToList();
-
+            ChairLv.ItemsSource = DBConnection.practise320_KudashovaAnnaEntities.Chair.ToList();
+            //faculties = new List<Faculty>(DBConnection.practise320_KudashovaAnnaEntities.Faculty.ToList());
+            //workers = new List<Worker>(DBConnection.practise320_KudashovaAnnaEntities.Worker.ToList());
+            //chairs = DBConnection.practise320_KudashovaAnnaEntities.Chair.Where(x => x.ID == DBConnection.loginedUser.ID_Chair).ToList();
+            NameZavTB.Text = DBConnection.loginedUser.Fullname;
             loggedUser = DBConnection.loginedUser;
  
             this.DataContext = this;
@@ -46,12 +45,6 @@ namespace SchoolKudasshova320.Pages
         NavigationService.GoBack();
         }
 
-
-        public void Refresh()
-        {
-            ChairLv.ItemsSource = chairs;
-        }
-
         private void ChairLv_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (ChairLv.SelectedItem is Chair chair)
@@ -59,11 +52,6 @@ namespace SchoolKudasshova320.Pages
                 ChairLv.SelectedItem = null;
                 NavigationService.Navigate(new AddDepartmentPage(chair));
             }
-        }
-
-        private void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-            Refresh();
         }
     }
 }
